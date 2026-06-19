172fc95d352433f85f29145895de71f9fd3350e9
wilmetjph <wilmetjph@gmail.com>
2026-06-19 19:08:56 +0200

    Add files via upload

---
+++ PHP/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Jean Wilmet(de0db7a0) - Broken Rock Paper Scissors</title>
+</head>
+<body>
+<div class="container">
+<h1>Welcome to Broken Rock Paper Scissors</h1>
+<p><strong>Note:</strong> This sample code is only
+partially done and serves only as a starting point for the assignment.
+</p>
+<p>
+<a href="login.php">Please Log In</a>
+</p>
+<p>
+Attempt to go to 
+<a href="game.php">game.php</a> without logging in - it should fail with an error message.
+</p>
+</div>
+</body>
+</html>
